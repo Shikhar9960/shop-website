22bbb631b38f922f7c69118e8c71b44c1847c227
Shikhar9960 <144546090+Shikhar9960@users.noreply.github.com>
2024-01-17 22:56:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,170 @@
+console.log("hello bachooo.................");
+
+
+let a = document.querySelector("#home")
+let b = document.querySelector("#about")
+
+let c = document.querySelector("#facebook")
+let d = document.querySelector("#insta")
+let e = document.querySelector("#gmail")
+let f = document.querySelector("#youtube")
+
+let g = document.querySelector("#led");
+let h = document.querySelector("#lcd");
+let i = document.querySelector("#inverter");
+let j = document.querySelector("#stabalizer");
+
+let k = document.querySelector("#num");
+let l = document.querySelector("#email");
+let la = document.querySelector("#address");
+
+let m = document.querySelector("#searchbtn");
+let n = document.querySelector("#submited");
+
+
+let o = document.querySelector("#whatsapp");
+// let p = document.querySelector("#container7");
+// let q = document.querySelector("#container8");
+let r = document.querySelector("#hours");
+
+
+
+m.addEventListener("click", () => {
+    alert("Eror 404")
+});
+
+n.addEventListener("click", () => {
+    alert("submitted")
+});
+
+
+
+
+
+
+
+
+
+a.addEventListener("click", () => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+b.addEventListener("click", () => {
+    window.scroll({
+        top: 400,
+        behavior: "smooth",
+    });
+});
+
+
+
+
+
+
+
+
+c.addEventListener("click", () => {
+    window.location.href = 'https://www.facebook.com/';
+});
+
+d.addEventListener("click", () => {
+    window.location.href = 'https://www.instagram.com/';
+});
+
+e.addEventListener("click", () => {
+    window.location.href = 'https://www.gmail.com/';
+});
+
+f.addEventListener("click", () => {
+    window.location.href = 'https://www.youtube.com/';
+});
+
+
+
+
+
+
+
+
+
+g.addEventListener("click", () => {
+    window.scroll({
+        top: 2800,
+        behavior: "smooth",
+    });
+});
+
+h.addEventListener("click", () => {
+    window.scroll({
+        top: 2800,
+        behavior: "smooth",
+    });
+});
+
+i.addEventListener("click", () => {
+    window.scroll({
+        top: 2000,
+        behavior: "smooth",
+    });
+});
+
+j.addEventListener("click", () => {
+    window.scroll({
+        top: 2300,
+        behavior: "smooth",
+    });
+});
+
+
+
+
+k.addEventListener("click", () => {
+    window.parent.location.href = "tel:+919305190601"
+});
+
+
+l.addEventListener("click", () => {
+    window.location.href = 'https://www.gmail.com/';
+});
+
+
+la.addEventListener("click", () => {
+    window.parent.location.href = "https://maps.app.goo.gl/dsFj1UUtV9Av28Q66"
+});
+
+
+o.addEventListener("click", () => {
+    window.location.href = 'https://wa.me/+919305190601?text=hey%20%2C%20Welcome%20to%20Sharad%20Electronics%20and%20Electrical';
+});
+
+
+
+
+
+
+
+
+r.addEventListener("click", () => {
+    window.scroll({
+        top: 1500,
+        behavior: "smooth",
+    });
+});
+
+
+
+// l.addEventListener("click", () => {
+//     let hey = confirm("Are You Sure Want to Download");
+//     if (hey == true) {
+//         window.location.href = 'https://docs.google.com/document/d/1BmcIflCqPgMF7K2kvr-2eDfJ-yokVHTj/edit?usp=drive_link&ouid=106356354253746696294&rtpof=true&sd=true';
+//     }
+//     else {
+//         alert("you don't want to download resume")
+//     }
+// });
+
+
+
